1f1454ae8c61c3827d72e65c2b01a3d017cbfd0c
ugurgog <32577192+ugurgog@users.noreply.github.com>
2019-02-18 23:26:49 +0300

    initial check

--- app/src/main/java/uren/com/ilgincbilgiler/Fragments/MainFragment.java
+++ app/src/main/java/uren/com/ilgincbilgiler/Fragments/MainFragment.java
@@ -1,11 +1,14 @@
 package uren.com.ilgincbilgiler.Fragments;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.NestedScrollView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -15,6 +18,7 @@ import com.google.android.gms.ads.MobileAds;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import uren.com.ilgincbilgiler.GeneralUtils.AdMobUtils;
+import uren.com.ilgincbilgiler.GeneralUtils.ShapeUtil;
 import uren.com.ilgincbilgiler.MainFragments.BaseFragment;
 import uren.com.ilgincbilgiler.R;
 
@@ -25,9 +29,48 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
 
     @BindView(R.id.adView)
     AdView adView;
-    @BindView(R.id.nestedSv)
-    NestedScrollView nestedSv;
 
+    @BindView(R.id.imgvUzay)
+    ImageView imgvUzay;
+    @BindView(R.id.imgvGelenekler)
+    ImageView imgvGelenekler;
+    @BindView(R.id.imgvHuman)
+    ImageView imgvHuman;
+    @BindView(R.id.imgvAnimal)
+    ImageView imgvAnimal;
+    @BindView(R.id.imgvDeepWeb)
+    ImageView imgvDeepWeb;
+    @BindView(R.id.imgvHistory)
+    ImageView imgvHistory;
+    @BindView(R.id.imgvBilim)
+    ImageView imgvBilim;
+    @BindView(R.id.imgvCinsellik)
+    ImageView imgvCinsellik;
+    @BindView(R.id.imgvDinler)
+    ImageView imgvDinler;
+    @BindView(R.id.imgvGenel)
+    ImageView imgvGenel;
+
+    @BindView(R.id.llUzay)
+    LinearLayout llUzay;
+    @BindView(R.id.llGelenekler)
+    LinearLayout llGelenekler;
+    @BindView(R.id.llHuman)
+    LinearLayout llHuman;
+    @BindView(R.id.llAnimal)
+    LinearLayout llAnimal;
+    @BindView(R.id.llDeepWeb)
+    LinearLayout llDeepWeb;
+    @BindView(R.id.llHistory)
+    LinearLayout llHistory;
+    @BindView(R.id.llBilim)
+    LinearLayout llBilim;
+    @BindView(R.id.llCinsellik)
+    LinearLayout llCinsellik;
+    @BindView(R.id.llDinler)
+    LinearLayout llDinler;
+    @BindView(R.id.llGenel)
+    LinearLayout llGenel;
 
     public MainFragment() {
     }
@@ -59,11 +102,39 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
     private void init() {
         MobileAds.initialize(getContext(), getActivity().getResources().getString(R.string.ADMOB_APP_ID));
         AdMobUtils.loadBannerAd(adView);
+
+        llUzay.setOnClickListener(this);
+        llGelenekler.setOnClickListener(this);
+        llHuman.setOnClickListener(this);
+        llAnimal.setOnClickListener(this);
+        llDeepWeb.setOnClickListener(this);
+        llHistory.setOnClickListener(this);
+        llBilim.setOnClickListener(this);
+        llCinsellik.setOnClickListener(this);
+        llDinler.setOnClickListener(this);
+        llGenel.setOnClickListener(this);
     }
 
     private void setImages() {
+        setItemImage(R.drawable.space_bg, imgvUzay);
+        setItemImage(R.drawable.traditional_bg, imgvGelenekler);
+        setItemImage(R.drawable.human_bg, imgvHuman);
+        setItemImage(R.drawable.animal_bg, imgvAnimal);
+        setItemImage(R.drawable.deep_web_bg, imgvDeepWeb);
+        setItemImage(R.drawable.history_bg, imgvHistory);
+        setItemImage(R.drawable.science_bg, imgvBilim);
+        setItemImage(R.drawable.sexuality_bg, imgvCinsellik);
+        setItemImage(R.drawable.religion_bg, imgvDinler);
+        setItemImage(R.drawable.general_bg, imgvGenel);
+    }
 
-        nestedSv.setBackgroundColor(getResources().getColor(R.color.Black, null));
+    private void setItemImage(int itemId, ImageView imageView){
+        Glide.with(getContext())
+                .load(itemId)
+                .apply(RequestOptions.circleCropTransform())
+                .into(imageView);
+        imageView.setBackground(ShapeUtil.getShape(getResources().getColor(R.color.transparent, null),
+                getResources().getColor(R.color.White, null), GradientDrawable.OVAL, 50, 2));
     }
 
     private void setShapes() {
@@ -73,5 +144,44 @@ public class MainFragment extends BaseFragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+        if (v == llUzay) {
+
+        }
+
+        if (v == llGelenekler) {
+
+        }
+
+        if (v == llHuman) {
+
+        }
+
+        if (v == llAnimal) {
+
+        }
+
+        if (v == llDeepWeb) {
+
+        }
+
+        if (v == llHistory) {
+
+        }
+
+        if (v == llBilim) {
+
+        }
+
+        if (v == llCinsellik) {
+
+        }
+
+        if (v == llDinler) {
+
+        }
+
+        if (v == llGenel) {
+
+        }
     }
 }
